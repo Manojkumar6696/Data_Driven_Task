db1643acd2eaeffff0bbe2c87c4c2a450546b2f8
Manojkumar <140330455+Manojkumar6696@users.noreply.github.com>
2025-05-08 12:47:00 +0530

    Add files via upload

---
+++ Test_Data/data.py
@@ -0,0 +1,10 @@
+"""
+This file contains basic data like url, file path, sheet
+"""
+
+class Data:
+
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    dashboard_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    excel_file = "D:\\python files\\Data_driven_task\\TestData\\test_data.xlsx"
+    sheet_number = "Sheet1"
